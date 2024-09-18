d9de2bfd0481c7d9877643c97956660b33b99ca3
Jeronimo Cardona <jerocardona312@gmail.com>
2024-09-18 18:12:21 -0500

    Admin Middleware

--- IMEC/bootstrap/app.php
+++ IMEC/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AdminAuthMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+    
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
